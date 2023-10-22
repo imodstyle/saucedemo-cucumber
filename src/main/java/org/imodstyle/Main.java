d1a9e5dba6c31a9e073f009edb84fb072ef2324d
imodstyle <donnyrahardika@gmail.com>
2023-10-22 19:48:00 +0700

    hello world

--- src/main/java/org/imodstyle/Main.java
+++ src/main/java/org/imodstyle/Main.java
@@ -3,5 +3,8 @@ package org.imodstyle;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("This is iModStyle");
+        System.out.println("This only for educational purpose only");
+        System.out.println("This package for testing saucedemo web using cucumber");
     }
 }
